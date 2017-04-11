2ccd8d643bfb07af56e2ab6c459bf6399114b275
Alex Thomassen <alex@thomassen.xyz>
2017-04-11 13:50:05 +0200

    Fix RSS feed (again) under /askfm/rss

--- app/Http/Controllers/AskfmController.php
+++ app/Http/Controllers/AskfmController.php
@@ -31,22 +31,34 @@ class AskfmController extends Controller
         $dom = new PHPHtmlParser\Dom;
         $dom->loadFromUrl('https://ask.fm/' . $user);
         $answers = $dom->find('.item-pager')->find('.streamItem-answer');
+
+        if (count($answers) === 0) {
+            $feed->title = 'Ask.fm - RSS Feed';
+            $feed->description = 'RSS feed covering answers of users on Ask.fm';
+            $date = new Carbon();
+            $msg = sprintf('User %s has not answered any questions.', $user);
+            $feed->add($msg, $msg, url($request->fullUrl()), $date->toAtomString(), $msg, $msg);
+            return $feed->render('atom');
+        }
+
         $feed->title = 'Ask.fm - ' . $user;
-        $feed->description = 'RSS feed showing answers of questions asked to: ' . $user;
+        $feed->description = 'RSS feed showing answers of Ask.fm questions asked to: ' . $user;
         $feed->link = $request->fullUrl();
         $feed->setDateFormat('datetime');
         $feed->lang = 'en';
-        $i = 0;
 
-        foreach($answers as $a) {
+        $ageStr = '.streamItemsAge a';
+        $date = $answers[0]->find($ageStr)->getAttribute('title');
+        $feed->pubdate = $date;
+
+        foreach ($answers as $a) {
+            $itemAge = $a->find($ageStr);
+
             $question = trim($a->find('.streamItemContent-question')->firstChild()->text);
             $answer = trim($a->find('.streamItemContent-answer')->firstChild()->text);
-            $link = "https://ask.fm" . $a->find('.streamItemsAge')->getAttribute('href');
-            $date = $a->find('.streamItemsAge')->getAttribute('title');
-            if($i === 0) {
-                $feed->pubdate = $date;
-                $i++;
-            }
+            $link = "https://ask.fm" . $itemAge->getAttribute('href');
+            $date = $itemAge->getAttribute('title');
+
             $feed->add($question, $user, $link, $date, $answer, $answer);
         }
         return $feed->render('atom');

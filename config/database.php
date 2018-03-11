04a4c94fdcf474310d9ca6dacf42782b9a49a88b
Alex Thomassen <alex@thomassen.xyz>
2018-03-12 00:28:54 +0100

    Update to Laravel 5.4

--- config/database.php
+++ config/database.php
@@ -112,16 +112,22 @@ return [
     */
 
     'redis' => [
+        'client' => 'predis',
 
-        'cluster' => false,
-
-        'default' => [
-            'host'     => env('REDIS_HOST', 'localhost'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port'     => env('REDIS_PORT', 6379),
-            'database' => 0,
+        'options' => [
+            'cluster' => 'redis',
         ],
 
+        'clusters' => [
+            'default' => [
+                [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => 0,
+                ],
+            ],
+        ],
     ],
 
 ];

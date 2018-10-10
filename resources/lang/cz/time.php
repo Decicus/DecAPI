1260e2dd5aa0623f309ea6dad636b2d8a75aadda
Alex Thomassen <alex@thomassen.xyz>
2018-10-10 17:46:21 +0200

    Add Czech translation for time strings, provided by Davidd#9915

---
+++ resources/lang/cz/time.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Český překlad času, například: /twitch/followage & /twitch/uptime
+ * Czech translation for time - Provided by Davidd#9915 on Discord
+ */
+return [
+    'year' => ':value rok|:value let',
+    'month' => ':value měsíc|:value měsíců',
+    'week' => ':value týden|:value týdnů',
+    'day' => ':value den|:value dnů',
+    'hour' => ':value hodinu|:value hodin',
+    'minute' => ':value minutu|:value minut',
+    'second' => ':value sekundu|:value sekund',
+    'invalid_timezone' => 'Bylo zadáno neplatné časové pásmo: :timezone',
+];

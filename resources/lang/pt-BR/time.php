dff63eb8c1953341420f94def7857cb37540dddc
Alex Thomassen <alex@thomassen.xyz>
2018-11-10 22:38:42 +0100

    Add translation for Portugese (Brazil)

---
+++ resources/lang/pt-BR/time.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Portuguese (Brazil) translation strings for time, for example: /twitch/followage & /twitch/uptime
+ *
+ * Thanks to: AlmeckySan#3786 (Discord)
+ */
+return [
+    'year' => ':value ano|:value anos',
+    'month' => ':value mês|:value meses',
+    'week' => ':value semana|:value semanas',
+    'day' => ':value dia|:value dias',
+    'hour' => ':value hora|:value horas',
+    'minute' => ':value minuto|:value minutos',
+    'second' => ':value segundo|:value segundos',
+    'invalid_timezone' => 'Invalid timezone specified: :timezone',
+];

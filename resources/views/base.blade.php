b642e139592b970412be23ab6ff1fd804a11935a
Alex Thomassen <alex@thomassen.xyz>
2019-04-23 22:09:41 +0200

    [Maintenance] Upgrade from Laravel 5.6 to 5.8

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,7 +7,7 @@
         {{-- <link rel="stylesheet" href="/css/font-awesome.min.css" type="text/css" /> --}}
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
         <link rel="stylesheet" href="/css/custom.css" type="text/css" />
-        <title>{{ env('SITE_TITLE', 'DecAPI') }} | {{ $page or '[Undefined]' }}</title>
+        <title>{{ env('SITE_TITLE', 'DecAPI') }} | {{ $page ?? '[Undefined]' }}</title>
     </head>
     <body>
         @yield('main')

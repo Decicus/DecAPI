a6cb16aaca49cb83a39788d11936c830c156c534
Alex Thomassen <alex@thomassen.xyz>
2019-07-01 02:49:49 +0200

    [Twitch API - Helix] Modify refreshing of users to refresh OAuth tokens using refresh token
    [Database] Fix migration on users table (refresh_token column)

--- database/migrations/2019_04_30_112531_modify_users_table_to_add_refresh_token.php
+++ database/migrations/2019_04_30_112531_modify_users_table_to_add_refresh_token.php
@@ -14,7 +14,7 @@ class ModifyUsersTableToAddRefreshToken extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('refresh_token')
+            $table->string('refresh_token', 400)
                   ->nullable()
                   ->after('access_token');
 

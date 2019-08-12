93e59685d1653d7247a4cc2388f5eba0890186c6
cristisilaghi <cristian.silaghi@mozilla.ro>
2019-08-12 20:13:47 +0300

    Romanian translation

---
+++ resources/lang/ro/time.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Romanian translation strings for time, for example: /twitch/followage & /twitch/uptime
+ */
+return [
+    'year' => '{1} :value an|[0,19] :value ani|[20,60] :value de ani',
+    'month' => '{1} :value lună|[0,19] :value luni|[20,60] :value de luni',
+    'week' => '{1} :value săptămână|[0,19] :value săptămâni|[20,60] :value de săptămâni',
+    'day' => '{1} :value zi|[0,19] :value zile|[20,60] :value de zile',
+    'hour' => '{1} :value oră|[0,19] :value ore|[20,60] :value de ore',
+    'minute' => '{1} :value minut|[0,19] :value minute|[20,60] :value de minute',
+    'second' => '{1} :value secundă|[0,19] :value secunde|[20,60] :value de secunde',
+    'invalid_timezone' => 'Fusul orar specificat este nevalid: :timezone',
+];

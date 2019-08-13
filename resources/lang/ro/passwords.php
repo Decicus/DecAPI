ed5fa29b5598417220998b9f4858dc3c495774eb
cristisilaghi <cristian.silaghi@mozilla.ro>
2019-08-13 14:51:38 +0300

    Romanian translation

---
+++ resources/lang/ro/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Parolele trebuie să aibă cel puțin șase caractere și să corespundă confirmării.',
+    'reset' => 'Parola ți-a fost resetată!',
+    'sent' => 'Ți-am trimis prin e-mail linkul de resetare a parolei!',
+    'token' => 'Acest token de resetare a parolei este nevalid.',
+    'user' => "Nu putem găsi un utilizator cu acea adresă de e-mail.",
+
+];

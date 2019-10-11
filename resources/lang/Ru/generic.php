472295c0171e3b149b5261923dd17879f4efe42d
Krper1111 <31844440+Krper1111@users.noreply.github.com>
2019-10-11 18:25:16 +0300

    initial translation

---
+++ resources/lang/Ru/generic.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * English translation strings for generic errors/status messages.
+ */
+return [
+    'username_required' => 'Укажите имя пользователя.',
+    'channel_name_required' => 'Укажите имя канала.',
+    'user_channel_name_required' => 'Укажите имя пользователя и канала.',
+    'max_count' => 'Значение не может быть больше :value.',
+    'invalid_limit' => 'Указано неверное значение "limit": :limit',
+    'invalid_offset' => 'Указано неверное значение "offset": :offset',
+    'error_loading_data' => 'Произошла ошибка при пролучении данных.',
+    'error_loading_data_api' => 'Произошла ошибка при получении данных через API.',
+];

9f200fbe0ad0757c39199fce881db83c626223f6
Krper1111 <31844440+Krper1111@users.noreply.github.com>
2019-10-11 19:12:11 +0300

    translate time to ru

---
+++ resources/lang/Ru/time.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Russian translation of uptime and followage
+ * created by rhizoids of kriper1111
+ * can't figure out how to specify modulus (if even possible) (for example "1", "21", "31", "61", etc. should have the same translation)
+ * better than nothing
+ */
+return [
+    'year' => '{1} :value год|[2,4] :value года|[5,*] :value лет',
+    'month' => '{1} :value месяц|[2,4] :value месяца|[5,*] :value месяцев',
+    'week' => '{1} :value неделю|[2,4] :value недели|[5,*] :value недель',
+    'day' => '{1} :value день|[2,4] :value дня|[5,*] :value дней',
+    'hour' => '{1} :value час|[2,4] :value часа|[5,*] :value часов',
+    'minute' => '{1} :value минуту|[2,4] :value минуты|[5,*] :value минут',
+    'second' => '{1} :value секунду|[2,4] :value секунды|[5,*] :value секунд',
+    'invalid_timezone' => 'Указано неверное значение часового пояса: :timezone',
+];

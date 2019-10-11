01e4dd05ab2ae7b3c55f11524957de94d88b6a11
Krper1111 <31844440+Krper1111@users.noreply.github.com>
2019-10-11 18:20:45 +0300

    Update auth.php

--- resources/lang/Ru/auth.php
+++ resources/lang/Ru/auth.php
@@ -1,1 +1,20 @@
 
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Введённые учётные данные не соответствуют нашим записям.',
+    'throttle' => 'Слишком много попыток входа. Пожалуйста, повторите ещё раз через :seconds секунд.',
+
+];

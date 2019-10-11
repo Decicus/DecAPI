bfc445152886249a3446d280e16e3bc910a11449
Krper1111 <31844440+Krper1111@users.noreply.github.com>
2019-10-11 20:52:56 +0300

    update plurals and shorten timezone exception
    let's hope that this'll work

--- resources/lang/ru/time.php
+++ resources/lang/ru/time.php
@@ -6,12 +6,12 @@
  * better than nothing
  */
 return [
-    'year' => '{1} :value год|[2,4] :value года|[5,*] :value лет',
-    'month' => '{1} :value месяц|[2,4] :value месяца|[5,*] :value месяцев',
-    'week' => '{1} :value неделю|[2,4] :value недели|[5,*] :value недель',
-    'day' => '{1} :value день|[2,4] :value дня|[5,*] :value дней',
-    'hour' => '{1} :value час|[2,4] :value часа|[5,*] :value часов',
-    'minute' => '{1} :value минуту|[2,4] :value минуты|[5,*] :value минут',
-    'second' => '{1} :value секунду|[2,4] :value секунды|[5,*] :value секунд',
-    'invalid_timezone' => 'Указано неверное значение часового пояса: :timezone',
+    'year' => ':value год|:value года|:value лет',
+    'month' => ':value месяц|:value месяца|:value месяцев',
+    'week' => ':value неделю|:value недели|:value недель',
+    'day' => ':value день|:value дня|:value дней',
+    'hour' => ':value час|:value часа|:value часов',
+    'minute' => ':value минуту|:value минуты|:value минут',
+    'second' => ':value секунду|:value секунды|:value секунд',
+    'invalid_timezone' => 'Указан неверный часовой пояс: :timezone',
 ];

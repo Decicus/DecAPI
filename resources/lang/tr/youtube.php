f6e4152d33cca57434eedc5cdae3b5ef1b081e3f
Hasan Ali Balcioglu <h.alibalcioglu@gmail.com>
2020-12-11 18:09:38 +0300

    Turkish Translation

---
+++ resources/lang/tr/youtube.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Turkish translation strings for YouTube-related endpoints.
+ */
+
+return [
+    /**
+     * For /youtube/latest_video when someone specifies an
+     * invalid channel ID (/channel/ URLs) or user (/user/ URLs).
+     */
+    'invalid_channel_value' => 'Geçersiz Youtube kanalı tanımlandı (:type) tanımlanan: :id',
+];

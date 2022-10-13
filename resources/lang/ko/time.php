34bb027866cc58f18ef956656af2fb563a1a3388
relationlife <relationlife@gmail.com>
2022-10-13 13:12:05 -0500

    time.php
    Added Korean translation for time API such as uptime, followage.

---
+++ resources/lang/ko/time.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Korean translation strings for time, for example: /twitch/followage & /twitch/uptime
+ * Translated by: Taewook Yang (RelationLife)
+ */
+return [
+    'year' => ':value 년',
+    'month' => ':value 개월',
+    'week' => ':value 주',
+    'day' => ':value 일',
+    'hour' => ':value 시간',
+    'minute' => ':value 분',
+    'second' => ':value 초',
+    'invalid_timezone' => '잘못된 시간대: :timezone',
+];

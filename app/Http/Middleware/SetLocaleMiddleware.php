81c514bdcad10b7bdc7e99fc65af4168bdc02408
Alex Thomassen <alex@thomassen.xyz>
2022-12-29 19:54:59 +0000

    [Locale] Fix locale validation logic

--- app/Http/Middleware/SetLocaleMiddleware.php
+++ app/Http/Middleware/SetLocaleMiddleware.php
@@ -8,6 +8,49 @@ use Closure;
 class SetLocaleMiddleware
 {
     /**
+     * Valid locales
+     *
+     * @var array
+     */
+    protected array $locales = [];
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $localeDirs = scandir(resource_path('lang'));
+
+        $this->locales = array_filter($localeDirs, function($dir) {
+            return !in_array($dir, ['.', '..']);
+        });
+    }
+
+    /**
+     * Validate locale from request parameter
+     *
+     * @param string $locale
+     *
+     * @return string The locale itself or the default locale (en).
+     */
+    private function getValidLocale(string $locale) : string
+    {
+        $locale = strtolower(trim($locale));
+        if (in_array($locale, $this->locales)) {
+            return $locale;
+        }
+
+        $locale = substr($locale, 0, 2);
+        if (in_array($locale, $this->locales)) {
+            return $locale;
+        }
+
+        return 'en';
+    }
+
+    /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -16,10 +59,7 @@ class SetLocaleMiddleware
      */
     public function handle($request, Closure $next)
     {
-        /**
-         * Filter away invalid characters from specified locale
-         */
-        $locale = preg_replace('[^A-z-]', '', $request->input('lang', 'en'));
+        $locale = $this->getValidLocale($request->input('lang', 'en'));
         App::setLocale($locale);
 
         return $next($request);
